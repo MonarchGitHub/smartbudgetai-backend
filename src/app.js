8455383a1a349d8e58b47cf4d75058464fd9ee4c
Monarch <monarchk7@gmail.com>
2024-08-29 22:27:55 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: '🔥'
+    })
+})
