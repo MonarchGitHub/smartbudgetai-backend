f040b005a9696a96aa49042015ca0f5da3c86c8b
Monarch <monarchk7@gmail.com>
2024-10-10 23:12:34 +0530

    added controllers

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -17,17 +17,38 @@ async function createUser(uid, name, phone) {
     }
 
     try {
+        const currentMonth = new Date().toLocaleString('default', { month: 'long' });
+        const currentYear = new Date().getFullYear();
+
+
+        const budgetData = {
+            month: currentMonth,
+            year: currentYear,
+            income: 0,
+            expenses: 0,
+            savings: 0,
+            budget: 0,
+            category: {
+                food: 0,
+                academics: 0,
+                entertainment: 0,
+                clothes: 0,
+            },
+        };
 
         const usersCollection = collection(db, 'users');
-
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
         const userData = {
             name: name,
-            phone: phone,
+            phone: phone
         };
         await setDoc(doc(db, "users", uid), {
             userData
         });
-        console.log('User created in database:', uid);
+        // await setDoc(doc(budgetCollectionRef, `01-2024`), budgetData);
+        await setDoc(doc(budgetCollectionRef, `${currentMonth}-${currentYear}`), budgetData);
+        // console.log('User created in database:', uid);
 
         return true;
     } catch (error) {
--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,43 +1,171 @@
-const { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } = require('firebase/firestore');
+const { getUserByEmail } = require('../auth/authUtil');
+const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
+
+
+
+const { collection, getDoc, addDoc, doc, updateDoc, deleteDoc } = require('firebase/firestore');
 const { db } = require('../config/firebase');
 
-// Function to set a new expense
-async function setExpense(expenseData) {
+async function getBudget(uid, month, year) {
     try {
-        const expensesCollection = collection(db, 'expenses');
-        const docRef = await addDoc(expensesCollection, expenseData);
-        console.log(`Expense added with ID: ${docRef.id}`);
-        return docRef.id;
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        const budgetDocSnap = await getDoc(budgetDocRef);
+
+        if (budgetDocSnap.exists()) {
+            console.log(`Budget fetched for ${month} ${year} for user ${uid}`);
+            return budgetDocSnap.data();
+        } else {
+            console.log(`No budget found for ${month} ${year} for user ${uid}`);
+            return null;
+        }
     } catch (error) {
-        console.error('Error adding expense: ', error);
+        console.error('Error fetching budget:', error);
+        return null;
     }
 }
-
-// Function to update an existing expense
-async function updateExpense(expenseId, updatedExpenseData) {
+async function updateBudget(uid, month, year, budgetData) {
     try {
-        const expenseDocRef = doc(db, 'expenses', expenseId);
-        await updateDoc(expenseDocRef, updatedExpenseData);
-        console.log(`Expense with ID ${expenseId} updated successfully`);
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        await updateDoc(budgetDocRef, budgetData);
+
+        console.log(`Budget updated for ${month} ${year} for user ${uid}`);
+
+        return true;
     } catch (error) {
-        console.error('Error updating expense: ', error);
+        console.error('Error updating budget:', error);
+        return false;
     }
 }
-
 // Function to delete an expense
-async function deleteExpense(expenseId) {
+async function addCategory(uid, month, year, categoryName, value) {
     try {
-        const expenseDocRef = doc(db, 'expenses', expenseId);
-        await deleteDoc(expenseDocRef);
-        console.log(`Expense with ID ${expenseId} deleted successfully`);
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        const budgetDocSnap = await getDoc(budgetDocRef);
+        if (budgetDocSnap.exists()) {
+            const budgetData = budgetDocSnap.data();
+            if (budgetData.category[categoryName] === undefined) {
+                budgetData.category[categoryName] = value;
+                const totalExpenses = Object.values(budgetData.category).reduce((acc, value) => acc + value, 0);
+                budgetData.expenses = totalExpenses;
+                await updateDoc(budgetDocRef, budgetData);
+                console.log(`Category ${categoryName} added for ${month} ${year} for user ${uid}`);
+                return true;
+            } else {
+                console.log(`Category ${categoryName} already exists for ${month} ${year} for user ${uid}`);
+                return false;
+            }
+        } else {
+            console.log(`No budget found for ${month} ${year} for user ${uid}`);
+            return false;
+        }
     } catch (error) {
-        console.error('Error deleting expense: ', error);
+        console.error('Error adding category:', error);
+        return false;
     }
 }
 
+async function updateCategory(uid, month, year, categoryName, newValue) {
+    try {
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        const budgetDocSnap = await getDoc(budgetDocRef);
+        if (budgetDocSnap.exists()) {
+            const budgetData = budgetDocSnap.data();
+            if (budgetData.category[categoryName] !== undefined) {
+                budgetData.category[categoryName] = newValue;
+                const totalExpenses = Object.values(budgetData.category).reduce((acc, value) => acc + value, 0);
+                budgetData.expenses = totalExpenses;
+                await updateDoc(budgetDocRef, budgetData);
+                console.log(`Category ${categoryName} updated for ${month} ${year} for user ${uid}`);
+                return true;
+            } else {
+                console.log(`Category ${categoryName} does not exist for ${month} ${year} for user ${uid}`);
+                return false;
+            }
+        } else {
+            console.log(`No budget found for ${month} ${year} for user ${uid}`);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error updating category:', error);
+        return false;
+    }
+}
+
+async function deleteCategory(uid, month, year, categoryName) {
+    try {
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        const budgetDocSnap = await getDoc(budgetDocRef);
+        if (budgetDocSnap.exists()) {
+            const budgetData = budgetDocSnap.data();
+            if (budgetData.category[categoryName] !== undefined) {
+                delete budgetData.category[categoryName];
+                const totalExpenses = Object.values(budgetData.category).reduce((acc, value) => acc + value, 0);
+                budgetData.expenses = totalExpenses;
+                await updateDoc(budgetDocRef, budgetData);
+                console.log(`Category ${categoryName} deleted for ${month} ${year} for user ${uid}`);
+                return true;
+            } else {
+                console.log(`Category ${categoryName} does not exist for ${month} ${year} for user ${uid}`);
+                return false;
+            }
+        } else {
+            console.log(`No budget found for ${month} ${year} for user ${uid}`);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error deleting category:', error);
+        return false;
+    }
+}
+
+async function getCategories(uid, month, year) {
+    try {
+        const usersCollection = collection(db, 'users');
+        const userDocRef = doc(db, 'users', uid);
+        const budgetCollectionRef = collection(userDocRef, 'budgets');
+        const budgetDocRef = doc(budgetCollectionRef, `${month}-${year}`);
+
+        const budgetDocSnap = await getDoc(budgetDocRef);
+        if (budgetDocSnap.exists()) {
+            const budgetData = budgetDocSnap.data();
+            const categories = budgetData.category;
+            console.log(`Categories fetched for ${month} ${year} for user ${uid}`);
+            return categories;
+        } else {
+            console.log(`No budget found for ${month} ${year} for user ${uid}`);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        return null;
+    }
+}
 // Export the functions
 module.exports = {
-    setExpense,
-    updateExpense,
-    deleteExpense,
+    updateBudget,
+    getBudget,
+    getCategories,
+    deleteCategory,
+    updateCategory,
+    addCategory
 };
